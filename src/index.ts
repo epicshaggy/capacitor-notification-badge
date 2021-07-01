7fbd43903628a256ba07a38b2325daa116413fb6
Jose Martinez <epicshaggy@gmail.com>
2021-07-01 09:39:53 -0400

    patch plugin not exported

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
+import { registerPlugin } from '@capacitor/core';
+import { NotificationBadgePlugin } from './definitions';
+
+const NotificationBadge = registerPlugin<NotificationBadgePlugin>(
+  'NotificationBadge',
+  {
+    web: () => import('./web').then(m => new m.NotificationBadgeWeb()),
+  },
+);
+
 export * from './definitions';
-export * from './web';
+export { NotificationBadge };
